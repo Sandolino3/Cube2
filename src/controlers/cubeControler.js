0b7c7df4d47cfef42b35481df41500da190c049e
Sandolino3 <89142522+Sandolino3@users.noreply.github.com>
2023-01-27 14:08:06 +0200

    add details button functionality

--- src/controlers/cubeControler.js
+++ src/controlers/cubeControler.js
@@ -1,25 +1,30 @@
 const router = require('express').Router();
-const cubes = require('../db.json')
-const fs = require('fs/promises')
-// const path = require('path')
-router.get('/create', (req,res)=>{
+const cubeService = require('../services/cubeService')
+
+router.get('/create', (req, res) => {
     res.render('create')
 })
 
-router.post("/create",(req,res)=>{
+router.post("/create",async (req, res) => {
     const cube = req.body
-    // console.log(path.relative('.'))
+
     if (cube.name.length < 2) {
         res.status(400).send('Name too short')
         return
     }
-    cubes.push(cube)
-    fs.writeFile('./src/db.json',JSON.stringify(cubes,'',4),{encoding:'utf-8'})
-        .then(()=>{
-            res.redirect('/')
-        })
-        .catch(err=>{
-            res.status(400).send(err)
-        })
+
+    try {
+        await cubeService.save(cube)
+        res.redirect('/')
+
+    } catch (err) {
+        res.status(400).send(err)
+    }
+
+})
+
+router.get('/:id', (req,res)=>{
+    const cube = cubeService.getOne(req.params.id)
+    res.render('details',{cube})
 })
 module.exports = router
