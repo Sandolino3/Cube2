c26ba3d333be5141e0faeef148cae7fa1864ad50
Sandolino3 <89142522+Sandolino3@users.noreply.github.com>
2023-03-11 17:55:51 +0200

    Add Accessory page

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const homecontroler = require('./controlers/homeControler')
 const cubeControler = require('./controlers/cubeControler')
+const acsesoryControler = require('./controlers/acsesoryControler')
 
 const router = express.Router()
 
 router.use('/', homecontroler)
 router.use('/cube',cubeControler)
+router.use('/acsesory', acsesoryControler)
 
 module.exports = router
